d59532b79f337ce4a7c5a72f577d463f322b381c
Kelly S <kxllys@gmail.com>
2019-01-07 01:29:48 +0800

    ey we good gang last slide n we done

--- jan4PPextended/ThirdViewController.swift
+++ jan4PPextended/ThirdViewController.swift
@@ -10,25 +10,13 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
- 
-    @IBOutlet weak var answerQuestion1: UITextField!
-    @IBOutlet weak var answerQuestion2: UITextField!
-    @IBOutlet weak var answerQuestion3: UITextField!
-    @IBOutlet weak var answerQuestion4: UITextField!
-    @IBOutlet weak var answerQuestion5: UITextField!
-    @IBOutlet weak var answerQuestion6: UITextField!
-    
-    @IBOutlet weak var answerQuestion7: UITextField!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
+        
+        
+        /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
